592c36f4f8b278e6b324a35eed29d623474f74be
IsoVoyd <dubnersamuel@gmail.com>
2021-02-09 09:21:59 -0800

    write SessionsController

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,18 @@
 class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if @user
+            login(@user)
+        else
+            render json: ["No matching credentials were found in the DB"], status: 404
+        end
+    end
+
+    def destroy
+        logout(@user)
+    end
 end
