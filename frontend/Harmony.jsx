19ade786e88797edd9e3778c3c0615df1da26fa3
IsoVoyd <dubnersamuel@gmail.com>
2021-02-09 16:16:03 -0800

    work on pushing to heroku

--- frontend/Harmony.jsx
+++ frontend/Harmony.jsx
@@ -3,13 +3,8 @@ import ReactDOM from "react-dom"
 import configureStore from "./store/store"
 import Root from "./components/root"
 
-// import { login, logout, signup } from "./actions/session_actions";
-
 document.addEventListener("DOMContentLoaded", (e) => {
     const store = configureStore();
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={store}/>, root);
-    // window.login = login;
-    // window.logout = logout;
-    // window.signup = signup;
 })
