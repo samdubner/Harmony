fc803fc06a12c297218e99a2f560ad55ebbd60de
IsoVoyd <dubnersamuel@gmail.com>
2021-02-10 10:34:10 -0800

    add 404 page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import MissingRoute from "./missingRoute"
 
 import LoginFormContainer from "./sessionForms/loginFormContainer"
 import SignupFormContainer from "./sessionForms/signupFormContainer"
@@ -16,6 +17,7 @@ class App extends React.Component {
                 <ProtectedRoute exact path="/" component={LoggedInContainer} />
                 <AuthRoute path="/signup" component={SignupFormContainer} />
                 <AuthRoute path="/login" component={LoginFormContainer} />
+                <Route path="/" component={MissingRoute} />
             </Switch>
         )
     }
---
+++ frontend/components/missingRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+class MissingRoute extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <h1>404</h1>
+        )
+    }
+}
+
+export default MissingRoute;
