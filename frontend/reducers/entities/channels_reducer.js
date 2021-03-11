ef650059111f40bde44fcc95248eaa5b3652ca9e
IsoVoyd <dubnersamuel@gmail.com>
2021-03-11 11:30:37 -0800

    add channels reducer

---
+++ frontend/reducers/entities/channels_reducer.js
@@ -0,0 +1,22 @@
+import {
+    RECEIVE_CHANNELS,
+    RECEIVE_CHANNEL,
+    DELETE_CHANNEL
+} from "../../actions/channel_actions"
+
+const channelsReducer = (prevState={}, action) => {
+    Object.freeze(prevState);
+
+    switch(action.type) {
+        case RECEIVE_CHANNELS:
+            return;
+        case RECEIVE_CHANNEL:
+            return;
+        case DELETE_CHANNEL:
+            return;
+        default:
+            return prevState;
+    }
+}
+
+export default channelsReducer;
