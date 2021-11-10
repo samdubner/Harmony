289f8472f2ec6e2d512eb19fa1521eb96eafbaa2
samdubner <dubnersamuel@gmail.com>
2021-11-09 16:51:37 -0800

    fix type in web_socket_server_url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.web_socket_server_url = "wss://hermonyclone.herokuapp.com/cable"
+  config.web_socket_server_url = "wss://harmonyclone.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = [ 'http://harmonyclone.herokuapp.com', 'https://harmonyclone.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
